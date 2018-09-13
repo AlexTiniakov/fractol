ab0a27688717dcbfb086da75297cebb9b3b19a69
Alex Tiniakov <39567406+AlexTiniakov@users.noreply.github.com>
2018-09-14 00:57:25 +0300

    Update threadF.c

--- src/threadF.c
+++ src/threadF.c
@@ -12,11 +12,11 @@
 
 #include <fractol.h>
 
-int ft_get_colour1(int i, int maxIterations, t_fr *fr)
+int ft_get_colour1(int i, int maxIterations, t_fr *fr)//try all like this one
 {
 	int colour;
 
-	colour = fr->col & (0xFFFFFF / (maxIterations - i));
+	colour = fr->col / i;
 	return (colour);
 }
 
@@ -50,21 +50,22 @@ int ft_get_colour4(int i, int maxIterations, t_fr *fr)
 	return (colour);
 }
 
-void* threadFunc1(void* thread_data)
+void* threadFunc1(void* thread_data)//optimize all like this one
 {
     t_fr *fr = (t_fr *)thread_data;
     int i;
-    int color;
     long double newRe, newIm, oldRe, oldIm;
     int x;
     int y = -1;
-    while (++y <= fr->h / 2)
+	int tab1 = fr->zoom * fr->w2 / 1.5;//to fr
+	int tab2 = fr->zoom * fr->w2;//to fr
+    while (++y <= fr->h2)
 	{
 		x = -1;
-		while (++x <= fr->w / 2)
+		while (++x <= fr->w2)
 		{
-			newRe = 1.5 * (x - fr->w / 2) / (0.5 * fr->zoom * fr->w) + fr->moveX;
-    		newIm = (y - fr->h / 2) / (0.5 * fr->zoom * fr->h) + fr->moveY;
+			newRe = (x - fr->w2) / fr->tab1 + fr->moveX;
+    		newIm = (y - fr->h2) / fr->tab2 + fr->moveY;
 			i = -1;
 			while (++i < fr->maxIterations)
 			{
@@ -73,14 +74,11 @@ void* threadFunc1(void* thread_data)
 				newRe = oldRe * oldRe - oldIm * oldIm + fr->cRe;
       			newIm = 2 * oldRe * oldIm + fr->cIm;
 				if((newRe * newRe + newIm * newIm) > 4)
+				{
+					ft_put_pixel1(x, y, fr, ft_get_colour1(i, fr->maxIterations, fr));
 					break;
+				}
 			}
-            if (i != fr->maxIterations)
-            {
-			color = ft_get_colour1(i, fr->maxIterations, fr);
-			//mlx_pixel_put(fr->mlx_ptr, fr->win_ptr, fr->x, fr->y, color);
-			ft_put_pixel1(x, y, fr, color);
-            }
 		}
 	}
     //ft_printf("1\n");
