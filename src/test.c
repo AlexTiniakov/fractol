e7e6bd282b0f10a5672a3ca57fab8ca4e4445129
Alex Tiniakov <39567406+AlexTiniakov@users.noreply.github.com>
2018-09-15 23:51:22 +0300

    Update test.c

--- src/test.c
+++ src/test.c
@@ -9,7 +9,7 @@
 /*   Updated: 2018/09/12 15:15:56 by otiniako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 screen(400, 300, 0, "Mandelbrot Set"); //make larger to see more detail!
 
   //each iteration, it calculates: newz = oldz*oldz + p, where p is the current pixel, and oldz stars at the origin
@@ -49,4 +49,12 @@ screen(400, 300, 0, "Mandelbrot Set"); //make larger to see more detail!
   //make the Mandelbrot Set visible and wait to exit
   redraw();
   sleep();
-  return 0;
+  return 0;*/
+
+#include <stdio.h>
+
+int main()
+{
+	long int a;
+	return(printf("%d\n", sizeof(a)));
+}
